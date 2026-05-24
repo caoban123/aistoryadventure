6803b7567a05aeb36811a70382f53523e8f8b767
root <root@LAPTOP-BSPGGOQB.localdomain>
2026-05-24 23:11:41 +0700

    update

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 (function () {
   const isLocal = !window.location.hostname.endsWith("aistoryadventure.xyz");
   window.AI_STORY_CONFIG = {
-    API_BASE: isLocal ? "http://127.0.0.1:8001" : "https://api.aistoryadventure.xyz",
+    API_BASE: isLocal ? "http://127.0.0.1:8002" : "https://api.aistoryadventure.xyz",
   };
 })();
